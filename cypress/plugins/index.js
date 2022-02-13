2cbd753ffe77057927dd0fbecf0b0a5a11ce0811
GobalakrishnanG <gobalakrishnan07@gmail.com>
2022-02-13 16:20:28 +0000

    updated allure config

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -18,7 +18,7 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
-const cucumber = require('cypress-cucumber-preprocessor').default
+const cucumber = require('cypress-cucumber-preprocessor').default;
 
 // module.exports = (on, config) => {
 //   on('file:preprocessor', cucumber())
